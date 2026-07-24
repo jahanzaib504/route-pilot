89cb7605881f5755fee7d6d8b02faac36e7f82f7
jahanzaib504 <jahanzaibawan2008@gmail.com>
2026-07-24 19:10:05 +0500

    Allowed host problem

--- backend/eldloggersite/settings.py
+++ backend/eldloggersite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4%fnk1q(g)+xy$r1jjq=edtalhq#@0hk#ct%t05@cj^c18y_l9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
