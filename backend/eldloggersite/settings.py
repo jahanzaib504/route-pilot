4e4f04be00cb580a2507055c4aad9e6e09a40c3a
jahanzaib504 <jahanzaibawan2008@gmail.com>
2026-07-24 20:31:53 +0500

    Resolve cors issue

--- backend/eldloggersite/settings.py
+++ backend/eldloggersite/settings.py
@@ -54,8 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    ".vercel.app"
 ]
 ROOT_URLCONF = 'eldloggersite.urls'
 
