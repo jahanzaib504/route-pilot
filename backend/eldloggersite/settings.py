73ca48c2013417b65b68ef8849ac2aa9b841bea1
jahanzaib504 <jahanzaibawan2008@gmail.com>
2026-07-24 20:36:18 +0500

    Resolving

--- backend/eldloggersite/settings.py
+++ backend/eldloggersite/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    ".vercel.app"
+    "https://route-pilot-pifr.vercel.app",
 ]
 ROOT_URLCONF = 'eldloggersite.urls'
 
